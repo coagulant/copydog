f9011d1ea51785783cf7ef1093befdd15f0b51cc
Ilya Baryshev <baryshev@gmail.com>
2012-06-25 01:43:10 +0400

    Trackers api object and more options for issues filtering

--- copydog/redmine.py
+++ copydog/redmine.py
@@ -37,16 +37,38 @@ class Redmine(object):
         :param page: page number (optional)
         :param offset: skip this number of issues in response (optional)
         :param limit: number of issues per page (optional)
-        :param project_id: get issues from the project with the given id, where id is either project id or project identifier
-        :param tracker_id: get issues from the tracker with the given id
+        :param project_id: get issues from the project with the given id,
+                           where id is either project id or project identifier (optional)
+        :param tracker_id: get issues from the tracker with the given id (optional)
+        :param tracker_name: get issues from the tracker with the given name (optional)
+        :param sort: column to sort with (optional)
+        :param inverse: inverse sorting (optional)
+        :type inverse: boolean
         """
+        if kwargs.pop('inverse', None):
+            if kwargs.get('sort'):
+                kwargs['sort'] += ':desc'
+
+        tracker_name = kwargs.pop('tracker_name', None)
+        if tracker_name:
+            if kwargs.get('tracker_id'):
+                raise RedmineException('Filter by tracker_id or tracker_name')
+            raise NotImplemented
+            kwargs['tracker_id'] = self.trackers[tracker_name]
+
         return self._get('issues', **kwargs)
 
     def projects(self):
         """ Get a list of projects
         """
         projects = self._get('projects')['projects']
-        return [Project(self, project_data) for project_data in projects]
+        return [Project(self, data) for data in projects]
+
+    def trackers(self):
+        """ Get a list of trackers
+        """
+        trackers = self._get('trackers')['trackers']
+        return [Tracker(self, data) for data in trackers]
 
 
 class ApiObject(object):
@@ -63,11 +85,13 @@ class ApiObject(object):
         return unicode(self).encode('utf-8')
 
     def __unicode__(self):
-        return u'<{0.__class__.__name__} {0.name}>'.format(self)
+        return u'<{class_name} {name}>'.format(class_name=self.__class__.__name__, name=self.name)
 
 
 class Project(ApiObject):
-    """ Project in Redmine"""
+    """ Redmine project"""
 
 
+class Tracker(ApiObject):
+    """ Redmine tracker"""
 
