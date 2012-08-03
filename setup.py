44187b9475491801adabe783dd002294818ef182
Ilya Baryshev <baryshev@gmail.com>
2012-08-04 00:54:48 +0400

    Setup.py improved, added licence

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from distutils.core import setup
+from copydog import __version__
+
 
 setup(
     name='copydog',
-    version='copydog.__version__',
-    packages=['copydog'],
+    version=__version__,
+    packages=['copydog', 'copydog.api', 'copydog.utils'],
     url='https://github.com/coagluant/copydog',
     license='MIT',
     author='coagulant',
     author_email='baryshev@gmail.com',
-    description='Copies issues between Redmine and Trello on the fly'
+    description='Copies issues between Redmine and Trello on the fly',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+    ]
 )
